bf5b0775d0e1ef324c196c52ba9b660d2978a088
hichbra <hicham.brahimi@laposte.net>
2016-10-19 19:13:36 +0200

    TimeSeries

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,68 @@
 
-/* TODO : Créer le modèle objet ici */ 
+/* TODO : Créer le modèle objet ici */
+export class Data {
+
+}
+
+
+export class TimeSeries extends Data {
+  constructor() {
+    super();
+    // On ne passe pas les valeurs par le constructeur pour pouvoir les vérifier
+    this.value = 0;
+    this.label = 0;
+  }
+  get values() {
+    return this.value || 0;
+  }
+  set values(values) {
+    let numeric = true;
+
+    for (let i = 0; i < values.length; i++) {
+      if (isNaN(parseFloat(values[i])) || !isFinite(values[i])) {
+        numeric = false;
+      }
+    }
+
+    if (this.label === 0) {
+      if (values.length > 1 && numeric) {
+        this.value = values;
+      } else {
+        this.value = 0;
+      }
+    } else if (values.length > 1 && values.length === this.label.length && numeric) {
+      this.value = values;
+    } else {
+      this.value = 0;
+    }
+  }
+  get labels() {
+    return this.label || 0;
+  }
+  set labels(labels) {
+    // 2016-10-19T08:00:00.000Z
+    const regex = new RegExp('20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]Z');
+    let date = true;
+
+    for (let i = 0; i < labels.length; i++) {
+      if (!regex.test(labels[i])) {
+        date = false;
+      }
+    }
+
+    if (this.value === 0) {
+      if (labels.length >= 1 && date) {
+        this.label = labels;
+      } else {
+        this.label = 0;
+      }
+    } else if (labels.length >= 1 && date && this.value.length === labels.length) {
+      this.label = labels;
+    } else {
+      this.label = 0;
+    }
+  }
+  toString() {
+    return (`(${this.value}, ${this.label})`);
+  }
+}
--- test/model/index.js
+++ test/model/index.js
@@ -1,10 +1,58 @@
 import expect from 'expect';
-
+import {
+  Data,
+  TimeSeries,
+} from '../../src/model';
 // import { ??? } from '../../src/model';
 
-import  { data }  from './sensors_data'
-console.log(data.length);
+import { data } from './sensors_data' ;
+
+console.log('Taille donnees : ', data.length);
 
 describe('Sensor model tests', () => {
-  /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
+  describe('TimeSeries', () => {
+    const TimeS = new TimeSeries();
+    it('values doit etre superieur a 1', () => {
+      // data[0].data.values
+      TimeS.values = [1];
+      expect(TimeS.values).toBe(0);
+    });
+
+    it('values doit etre un tableau', () => {
+      // data[0].data.values
+      TimeS.values = 1;
+      expect(TimeS.values).toBe(0);
+    });
+
+    it('values doit etre des chiffres', () => {
+      // data[0].data.values
+      TimeS.values = ['a', 'b'];
+      expect(TimeS.values).toBe(0);
+    });
+
+    it('labels doit etre superieur a 1', () => {
+      // data[0].data.labels
+      TimeS.labels = [1];
+      expect(TimeS.values).toBe(0);
+    });
+
+    it('labels doit etre des dates', () => {
+      // data[0].data.labels
+      TimeS.labels = [958, 5];
+      expect(TimeS.labels).toBe(0);
+    });
+
+    it('labels doit etre un tableau', () => {
+      // data[0].data.values
+      TimeS.labels = 1;
+      expect(TimeS.values).toBe(0);
+    });
+
+    it('il doit avoir le meme nombre de values et de labels', () => {
+      TimeS.labels = data[0].data.labels; // 9
+      TimeS.values = [23, 23, 22, 21, 23, 23, 23, 25]; // 8
+
+      expect(TimeS.values).toBe(0);
+    });
+  });
 });
